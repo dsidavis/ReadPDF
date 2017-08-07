434400a745f042b32c824a5dec84863c0b8ea88d
Duncan Temple Lang <duncan@r-project.org>
2017-08-07 07:32:46 -0700

    code for getting text by each font and for the most commonly used font, i.e. the text of the document

--- R/pdfItalics.R
+++ R/pdfItalics.R
@@ -43,7 +43,45 @@ function(doc)
 
 
 textByFont =
+    # Get all the text nodes for a single font identifier
 function(doc, font)
 {
    getNodeSet(doc, sprintf("//text[@font = '%s']", font))
+}
+
+
+textByFonts =
+    # Get all the text strings for each font in the document.
+function(doc)
+{
+    if(is.character(doc))
+        doc = readPDFXML(doc)
+
+    fontIds = unlist(getNodeSet(doc, "//fontspec/@id"))
+    txt = lapply(fontIds, function(id) sapply(textByFont(doc, id), xmlValue))
+    names(txt) = fontIds
+    txt
+}
+
+getDocFont = getTextFont =
+    #
+    # Get the font information for the most commonly used font in the document,
+    # which is assumed to be that of the text.
+    # There are cases in which the most common font may not be that of the text.
+    # 
+function(doc)
+{
+    txt = textByFonts(doc)
+    ctr = sapply(txt, function(x) sum(nchar(x)))
+    info = fontInfo(doc)
+    id = names(ctr)[which.max(ctr)]
+    info[id, ]
+} 
+
+
+f = 
+function(p, bbox = getBBox2(p), cols = getColPositions(p))
+{
+    kols = split(bbox, cut(bb[, "left"], c(0, cols, Inf) - 2))
+#    lapply(kols, function(x))
 }
