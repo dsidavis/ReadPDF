7e0b7e0e6f515d0842b08b808aa49ecbf6ff1846
Duncan Temple Lang <duncan@r-project.org>
2017-08-14 11:39:38 -0700

    rotate text based on @rotation in renderPage

--- R/plot.R
+++ R/plot.R
@@ -87,9 +87,14 @@ function(page, cex.text = .5, adj = c(0, 1), showText = TRUE, showBoxes = FALSE)
         if(length(txt)) {
             bb = t(sapply(txt, xmlAttrs))
             storage.mode(bb) = "double"
-
+#            bb = as.data.frame(bb)
+            
             colors = getNodeColors(txt)
-            text(bb[,2], h - bb[,1], sapply(txt, xmlValue), cex = cex.text, adj = adj, col = colors)
+            text = sapply(txt, xmlValue)
+            by(1:nrow(bb), bb[, "rotation"],
+               function(i)
+                 text(bb[i,2], h - bb[i,1], text[i], cex = cex.text, adj = adj, col = colors[i], srt = - bb[i[1], "rotation"])) 
+#            text(bb[,2], h - bb[,1], sapply(txt, xmlValue), cex = cex.text, adj = adj, col = colors) 
         }
     }
 
