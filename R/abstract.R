31aac9228759e8fa78685eb513dc876a723f9f32
Duncan Temple Lang <duncan@r-project.org>
2017-09-08 17:04:51 -0700

    some additional functionality for abstracts and experiments

--- R/abstract.R
+++ R/abstract.R
@@ -15,16 +15,35 @@ function(page, bbox = getBBox2(getNodeSet(page, ".//text[@rotation = 0]")))
 }
 
 findAbstract =
-function(doc, asNodes = TRUE,  page = doc[[1]])
+function(doc, asNodes = TRUE, page = doc[[1]], byLine = TRUE)
 {
     if(is.character(doc))
         doc = readPDFXML(doc)
+    
+#opdf(doc)    
 
-
+    if(isBioOne(doc) && missing(page))
+       page = doc[[2]]
+    
     nodes = list()
-    a = getNodeSet(page, ".//text[starts-with(lower-case(normalize-space(.)), 'abstract') or starts-with(lower-case(normalize-space(.)), 'summary')]")
-browser()
+    
+      # Start by finding a declaration of an abstract: Abstract or Summary
+    a = findAbstractDecl(page)
+#    browser()
+
+    kw = findKeywordDecl(page)
+    if(length(a) && length(kw)) {  #XXX Should check kw is below a
+        nodes = getNodesBetween(a[[1]], kw[[1]])
+        return(if(byLine) nodesByLine(nodes) else nodes)
+    }
+
+
+    rect = getNodeSet(page, ".//rect")
+    rect.bb = getBBox(rect)
+
+       # If no declaration
     if(length(a) == 0) {
+        # From Emerging Infectious Disease format.
         # See if there are two columns and a line across one column that 
         # separates some text from  regular document text
         # This is the title, author list, abstract, line and then regular text.
@@ -76,14 +95,64 @@ browser()
         }
     } else {
 
+         # So we have an Abstract declaration  (or summary, etc.)
+
         mar = margins(page)
         bb = getBBox2(a)
+        browser()
+        # See if we have a line just below the declaration and if so, see if there is one below the text.
+        bb2 = getBBox(getNodeSet(page, ".//line | .//rect"))
+        bb2 = bb2[ bb2[,2]> bb[1,2], ]
+        
 
-        # This test may be more appropriate as is there any text within a few lines that is
+        # This test may be more appropriate: asking if there any text nodes within a few lines that is
         # significantly to the left of a[[1]].  This is for the Elsevier documents
-        # such as Oliveira-2009
+        # such as Oliveira-2009.
+        # However, causes problems for Buckley-2006 and similar that have a Summary not quite centered
         if(anyTextToLeft(a[[1]], bb)) {  # Used to be simply if(bb[1,1] > mar[1] * 1.1)
-            # So not flush with left margin or even very close
+           nodes = getShiftedAbstract(page, bbox)
+        } else {
+            # Flush with left margin.  Maybe not quite flush.
+
+              # Dealt with this above now as first check.
+            kw = findKeywordDecl(page)
+            if(length(kw))
+                nodes = getNodesBetween(a[[1]], kw[[1]])
+            else
+               nodes = spansColumns(page, doc = doc)
+        }
+    }
+
+    if(byLine)
+       nodesByLine(nodes)
+    else
+       nodes
+}
+
+findKeywordDecl =
+    # Find Key Words, Keywords, etc. 
+    # Could add test for bold font.
+function(page)
+{
+    getNodeSet(page, ".//text[starts-with(., 'Key Words') or starts-with(., 'Keywords') or starts-with(., 'Key words')]")    
+}
+
+findAbstractDecl =
+    # Find a node that identifies an abstract or summary. Add more as needed.
+function(page)
+{
+    getNodeSet(page, ".//text[starts-with(lower-case(normalize-space(.)), 'abstract') or starts-with(lower-case(normalize-space(.)), 'summary')]")
+}
+
+anyTextToLeft =
+function(node, bbox = getBBox2(list(node)), page = xmlParent(node))
+{
+  length(getNodeSet(page, sprintf(".//text[ abs(@top - %f) < 30 and @left <=  %f]", bbox[1,2], bbox[1,1]*.66))) > 0
+}
+
+
+getShiftedAbstract =
+  # So not flush with left margin or even very close
             # e.g. 1.s2.0-S1090
             #
             # Check if it is centered
@@ -96,29 +165,13 @@ browser()
         # defines the end. Then go back and find all the text to right
             #
             # If the "Abstract" line is indented, then need to allow for how far back??
-            # Using 14.  Used to be 4. But we really should look at the next line down.
-            nodes = getNodeSet(page, sprintf(".//text[abs(@left - %d) < 14]", bb[1,1]))
-            bb2 = getBBox2(nodes)
-            bot = max(bb2[, 2] + bb[,4])
-            nodes = getNodeSet(page, sprintf(".//text[abs(@left - %d) < 14 and @top > %d and @top + @height <= %f + 9]", bb[1,1] - 4, bb[1,2] - 4, bot))       
-        } else {
-            # Flush with left margin
-
-            kw = getNodeSet(page, ".//text[starts-with(., 'Key Words') or starts-with(., 'Keywords') or starts-with(., 'Key words')]")
-            if(length(kw))
-                nodes = getNodesBetween(a[[1]], kw[[1]])
-            else
-               nodes = spansColumns(page, doc = doc)
-        }
-    }
-        
-    nodesByLine(nodes)
-}
-
-anyTextToLeft =
-function(node, bbox = getBBox2(list(node)), page = xmlParent(node))
+            # Using 14.  Used to be 4. But we really should look at the next line down.        
+function(page, bbox)
 {
-  length(getNodeSet(page, sprintf(".//text[ abs(@top - %f) < 30 and @left <=  %f]", bbox[1,2], bbox[1,1]*.66))) > 0
+    nodes = getNodeSet(page, sprintf(".//text[abs(@left - %d) < 14]", bbox[1,1]))
+    bb2 = getBBox2(nodes)
+    bot = max(bb2[, 2] + bbox[,4])
+    nodes = getNodeSet(page, sprintf(".//text[abs(@left - %d) < 14 and @top > %d and @top + @height <= %f + 9]", bbox[1,1] - 4, bbox[1,2] - 4, bot))       
 }
 
 
