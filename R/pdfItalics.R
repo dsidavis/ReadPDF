957a3092d8d5e7df9eda90fd87332ee81b073138
Duncan Temple Lang <duncan@r-project.org>
2017-10-16 19:08:10 -0700

    nuances about recognizing italics. Perhaps overfitting

--- R/pdfItalics.R
+++ R/pdfItalics.R
@@ -2,26 +2,28 @@ getItalics =
     #
     # get the text that are in italics.
     #
-function(doc = xmlParsePDFTOHTML("../../pdftohtml/examples/Italics.xml"), useHeuristics = TRUE)
+function(doc = xmlParsePDFTOHTML("../../pdftohtml/examples/Italics.xml"), useHeuristics = TRUE, isItalicOnly = FALSE)
 {
     if(is.character(doc))
        doc = xmlParsePDFTOHTML(doc)
     
     italics = getNodeSet(doc, "//fontspec[@isItalic = '1']")
+    
     if(length(italics) == 0 && !useHeuristics)
         return(NULL)
 
-    if(length(italics) == 0) {
+    if(length(italics) == 0 || !isItalicOnly) {
         # So using heuristics, i.e. the name of the font rather than the flags in the PDF for the font.
        fonts = getNodeSet(doc, "//fontspec")
        names = sapply(fonts, xmlGetAttr, "name")
-       w = grepl("I$|Italic|Itl", names)
-       italics = fonts[w]
+       w = grepl("IT?$|Italic|Itl|-i$", names)
+       italics = c(italics, fonts[w])
     }
 
     if(length(italics) == 0)
         return(NULL)
-    
+
+     # Go get the text for the nodes with these fonts.
     fid = sapply(italics, xmlGetAttr, "id")
 
     xpq = paste("//text[", paste( sprintf("@font = '%s'", fid), collapse = " or "), "]")
