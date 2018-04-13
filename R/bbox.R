d09ca7282a853ae79ead1b0570c36842d0272673
Duncan Temple Lang <duncan@r-project.org>
2018-04-13 06:48:15 -0700

    amongst other things, have getBBox optionally return the width and height, not the x1, y1.  Also add color to the result and other things

--- R/bbox.R
+++ R/bbox.R
@@ -1,21 +1,22 @@
 getBBox2 =
     # For text, not rect or line nodes.
-function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE)
+function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE, color = FALSE)
     UseMethod("getBBox2")
 
 getBBox2.PDFToXMLPage =
     # For text, not rect or line nodes.
-function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE)
-    getBBox2(getNodeSet(nodes, ".//text"), asDataFrame, color)
+function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE, color = FALSE)
+    getBBox2(getNodeSet(nodes, ".//text"), asDataFrame, color = color)
 
 
 getBBox2.XMLInternalNode =
-function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE)
-    getBBox2(list(nodes), asDataFrame, attrs, pages, rotation)
+function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE, color = FALSE)
+    getBBox2(list(nodes), asDataFrame, attrs, pages, rotation, color)
 
 getBBox2.XMLNodeSet = getBBox2.list = 
     # For text, not rect or line nodes.
-function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE)
+    #XXX Add support for color.
+function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rotation"), pages = FALSE, rotation = FALSE, color = FALSE)
 {
     if(is(nodes, "XMLInternalElementNode"))
         if(xmlName(nodes) == "text")
@@ -52,25 +53,27 @@ function(nodes, asDataFrame = FALSE, attrs = c("left", "top", if(rotation) "rota
 getBBox =
     #
     # This bbox function expects an attribute named bbox
-    #
-function(nodes, asDataFrame = FALSE, color = FALSE)
+    # This is for rect and line nodes, not <text> nodes. Use getBBox2() for that.
+function(nodes, asDataFrame = FALSE, color = FALSE, diffs = FALSE)
     UseMethod("getBBox")
 
 getBBox.XMLInternalNode =
-function(nodes, asDataFrame = FALSE, color = FALSE)    
-    getBBox(list(nodes), asDataFrame, color)
+function(nodes, asDataFrame = FALSE, color = FALSE, diffs = FALSE)    
+    getBBox(list(nodes), asDataFrame, color, diffs)
 
 getBBox.XMLNodeSet = getBBox.list =
     #
     # This bbox function expects an attribute named bbox
     #
-function(nodes, asDataFrame = FALSE, color = FALSE)    
+function(nodes, asDataFrame = FALSE, color = FALSE, diffs = FALSE)    
 {
     if(length(nodes) == 0) {
-        if(asDataFrame)
-            return(data.frame(x0 = numeric(), y0 = numeric(), x1 = numeric(), y1 = numeric()))
-        else
-            return(matrix(0, nrow = 0, ncol = 4 + if(color) 2 else 0, dimnames = list(NULL, c("x0", "y0", "x1", "y1", if(color) c("fill", "stroke")))))
+        ans = if(asDataFrame)
+                data.frame(x0 = numeric(), y0 = numeric(), x1 = numeric(), y1 = numeric())
+              else
+                 matrix(0, nrow = 0, ncol = 4 + if(color) 2 else 0, dimnames = list(NULL, c("x0", "y0", "x1", "y1", if(color) c("fill", "stroke"))))
+        if(diffs)
+            names(ans)[3:4] = c("width", "height")
     }
     
     tmp = sapply(nodes, xmlGetAttr, "bbox")
@@ -78,7 +81,7 @@ function(nodes, asDataFrame = FALSE, color = FALSE)
     bb = matrix(as.numeric(unlist(els)), , 4, byrow = TRUE)
     colnames(bb) = c("x0", "y0", "x1", "y1")
     ty = sapply(nodes, xmlName)
-    if(asDataFrame) {
+    ans = if(asDataFrame) {
         ans = as.data.frame(bb)
         ans$nodeType = ty
         ans
@@ -86,6 +89,23 @@ function(nodes, asDataFrame = FALSE, color = FALSE)
         rownames(bb) = sapply(nodes, xmlName)
         bb
     }
+
+    if(color) {
+        cols = lapply(c("fill.color", "stroke.color"), function(at) sapply(nodes, xmlGetAttr, at))
+        if(asDataFrame) {
+            ans$fill = cols[[1]]
+            ans$stroke = cols[[2]]            
+        } else {
+            ans = cbind(ans, cols[[1]], cols[[2]])
+        }
+    }
+
+    if(diffs) {
+        ans[3:4] = ans[3:4] - ans[1:2]
+        names(ans)[3:4] = c("width", "height")        
+    }
+
+    ans
 }
 
 
