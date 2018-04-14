ca9fcec0e5002ec7b3de1b3d42707d31c58e5c40
Duncan Temple Lang <duncan@r-project.org>
2018-04-14 07:25:45 -0700

    coercion method for character/filename to PDFToXMLDoc

--- R/AClasses.R
+++ R/AClasses.R
@@ -1,2 +1,5 @@
 setOldClass(c("PDFToXMLDoc", "ConvertedPDFDoc", "XMLInternalDocument", "XMLAbstractDocument"))
 setOldClass(c("PDFToXMLPage", "ConvertedPDFPage", "XMLInternalElement", "XMLInternalNode", "XMLAbstractNode"))
+
+
+setAs("character", "PDFToXMLDoc", function(from) readPDFXML(from))
