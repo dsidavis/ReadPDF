e1b80597758397b511e03ee79d27ac279e9ee149
Duncan Temple Lang <duncan@r-project.org>
2018-04-15 15:33:53 -0700

    make getColPositions() a little more robust. Still needs more work. But more flexibility here - local = TRUE allows us to compute doc font, etc. based on the entire document or single page

--- R/colPositions.R
+++ R/colPositions.R
@@ -1,5 +1,5 @@
 getXPathDocFontQuery =
-function(node, docFont = TRUE, fontId = getDocFont(node)$id)
+function(node, docFont = TRUE, fontId = getDocFont(node, local = local)$id, local = FALSE)
 {
     xp = if(is.logical(docFont) && docFont)
             sprintf("//text[@font = '%s']", fontId)
@@ -62,8 +62,8 @@ function(p, threshold = .1,
 getColPositions.PDFToXMLPage = getColPositions.XMLInternalNode =  getColPositions.XMLInternalElementNode =
     # For a single page
 function(p, threshold = .1,
-         txtNodes = getNodeSet(p, getXPathDocFontQuery(p, docFont)),
-         bbox = getBBox2(txtNodes), docFont = TRUE, align = "left", ...)    
+         txtNodes = getNodeSet(p, getXPathDocFontQuery(p, docFont, local = local)),
+         bbox = getBBox2(txtNodes), docFont = TRUE, align = "left", local = FALSE, ...)    
 {
     bbox = as.data.frame(bbox)
 
@@ -74,6 +74,11 @@ function(p, threshold = .1,
                  )
     tt = table(vals)
     
+    if(missing(txtNodes) && nrow(bbox) == 0 && !local) {
+        # Use the page-specific font count
+        return(getColPositions(p, threshold, docFont = docFont, align = align, local = TRUE, ...))
+    }
+
     # Subtract 2 so that we start slightly to the left of the second column to include those starting points
     # or change cut to be include.lowest = TRUE
     ans = as.numeric(names(tt [ tt > nrow(bbox)*threshold])) - 2
@@ -92,7 +97,15 @@ function(p, threshold = .1,
     
     w = which(diff(ans) < minDiff)
     if(length(w))
-       ans = ans[ - (w+1)]
+        ans = ans[ - (w+1)]
+
+
+    if(length(ans) == 1 && ans[1] > .4*dim(p)["width"]) {
+        ## So only one column and it starts close to the middle of the page. Probably means
+        ## there is one to the left that we didn't find. This may be because the text is in a different font.
+        ##
+        ans = c(margins(p)[1], ans)
+     }
     
     ans
 }
@@ -115,7 +128,10 @@ function(p, threshold = .1, txtNodes = getNodeSet(p, ".//text"), bbox = getBBox2
 
 getNumCols.character =
 function(p, threshold = .1, txtNodes = getNodeSet(p, ".//text"), bbox = getBBox2(txtNodes))
-  getNumCols(  readPDFXML(p), threshold, txtNodes, bbox)
+{
+    p = readPDFXML(p)
+    getNumCols(p , threshold, txtNodes, bbox)
+}
 
 
 getNumCols.PDFToXMLPage = getNumCols.XMLInternalNode =
