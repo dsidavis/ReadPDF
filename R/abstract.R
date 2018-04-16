9c450c613f002da35a1ac0f2df9f5e126801f6e3
Duncan Temple Lang <duncan@r-project.org>
2018-04-16 06:42:08 -0700

    extended margins() to handle document, character, page, and list of nodes

--- R/abstract.R
+++ R/abstract.R
@@ -10,9 +10,31 @@ margins =
     # Specifying @rotation = 0 for case when there is a rotate line of text identifying how
     # the document was retrieved, e.g. downloaded by UC Davis.....
     #XXX should we extend this to get top and bottom.
-    #XXX  and deal with a set of nodes, and also an entire document.
+    #XXX  and deal with a set of nodes, a page, and also an entire document.
 function(page, bbox = getBBox2(getNodeSet(page, ".//text[@rotation = 0]")))
+    UseMethod("margins")
+
+margins.character =
+function(page, bbox = getBBox2(getNodeSet(page, ".//text[@rotation = 0]")))        
 {
+    margins(readPDFXML(page))
+}
+
+margins.PDFToXMLDoc =
+function(page, bbox = getBBox2(getNodeSet(page, ".//text[@rotation = 0]")))        
+{
+    lapply(getPages(page), margins)
+}
+
+margins.XMLInternalNode = margins.PDFToXMLPage =
+function(page, bbox = getBBox2())
+{
+    margins(getNodeSet(page, ".//text[@rotation = 0]"))
+}
+
+margins.list = margins.XMLNodeSet =
+function(page, bbox = getBBox2(unlist(page)))
+{    
    c(min(bbox[, 1]), max(bbox[,1] + bbox[,3]))
 }
 
