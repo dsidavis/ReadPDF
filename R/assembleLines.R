41d65794bb4dd8f5d2d82eced6da47338215fc59
Duncan Temple Lang <duncan@r-project.org>
2018-05-10 18:59:53 -0700

    export getLines. Need to unify getPageLines, assembleLines and getLines

--- R/assembleLines.R
+++ R/assembleLines.R
@@ -3,35 +3,50 @@ doc = readPDFXML("Electric_A2A _DPL_08-19-13.xml")
 page = doc[[2]]
 #rr = getNodeSet(page, ".//rect")
 
-getHLines(page)
+hor = getHLines(page)
+plot(page)
+abline(h = dim(page)[2] - hor$y0, col = "red")
+
+vert = getHLines(page, horiz = FALSE)
+abline(v = unique(vert$x0), col = "red")
 }
 
-getHLines = 
+
+getLines = getHLines = 
 function(page, nodes = getNodeSet(page, ".//rect"), mar = margins(page),# page is global here!
          bb = getBBox(nodes, asDataFrame = TRUE), 
-         threshold = 5, lhThreshold = 6, minLineLength = 20)
+         threshold = 5, lhThreshold = 6, minLineLength = 20, marThreshold = 10, horiz = TRUE)
 {         
-  # horizontal lines
-    w = abs(bb$y1 - bb$y0) < lhThreshold # & (bb$x1 - bb$x0) > minLineLength
-    hbb = bb[w,]
+     # horizontal lines
+    w = if(horiz)
+           abs(bb$y1 - bb$y0) < lhThreshold 
+        else
+           abs(bb$x1 - bb$x0) < lhThreshold
     
-    uvals = unique(hbb$y1)
-    g = split(hbb, cut(hbb$y1, c(0, uvals)))
+    hbb = bb[w,]
 
-#    sapply(g, nrow)
+    var = if(horiz) "y1" else "x1"
+    
+    uvals = unique(hbb[[var]])
+    g = split(hbb, cut(hbb[[var]], c(0, uvals)))
 
-    wd = sapply(g, function(x) range(x$x0, x$x1))
-
-    w2 = wd[1,] < (mar[1] + 10) &  wd[2,] > ( mar[2] -  10)
+    if(horiz) {
+        wd = sapply(g, function(x) range(x$x0, x$x1))
+        w2 = wd[1,] < (mar[1] + marThreshold) &  wd[2,] > ( mar[2] -  marThreshold)
+    } else {
+        ht = sapply(g, function(x) diff(range(x$y0, x$y1)))
+        w2 = ht > dim(page)[2]*.1
+    }
 
     g = g[w2]
-    do.call(rbind, lapply(g, joinLines))
+    do.call(rbind, lapply(g, joinLines, horiz = horiz))
 }
 
 
 joinLines = 
 function(xx, horiz = TRUE, maxGap = 5, vars = if(horiz) c("x0", "x1") else c("y0", "y1"))
 {
+    xx = xx[ order(xx[[ vars[2] ]]), ]
     d = xx[[vars[1]]][-1] - xx[[vars[2]]] [-nrow(xx)]
   
     w = c(0, cumsum(d >= maxGap))
