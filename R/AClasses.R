52bf0073bcdf93525647f1e5bdc7156bcae771da
Duncan Temple Lang <duncan@r-project.org>
2019-08-06 16:03:12 -0700

    getting the S3/S4 class interaction sorted out gradually

--- R/AClasses.R
+++ R/AClasses.R
@@ -1,6 +1,17 @@
-setOldClass(c("PDFToXMLDoc", "ConvertedPDFDoc", "XMLInternalDocument", "XMLAbstractDocument"))
-setOldClass(c("PDFToXMLPage", "ConvertedPDFPage", "XMLInternalElement", "XMLInternalNode", "XMLAbstractNode"))
 
+#setOldClass(c("PDFToXMLDoc", "ConvertedPDFDoc", "XMLInternalDocument", "XMLAbstractDocument", "Document"))
+#setOldClass(c("PDFToXMLPage", "ConvertedPDFPage", "XMLInternalElement", "XMLInternalNode", "XMLAbstractNode"))
+
+
+if(TRUE) {
+setOldClass(c("ConvertedPDFDoc", "XMLInternalDocument", "XMLAbstractDocument"))
+setOldClass(c("ConvertedPDFPage", "XMLInternalElement", "XMLInternalNode", "XMLAbstractNode"))    
+setClass("PDFToXMLDoc", contains = c("ConvertedPDFDoc", "Document"))
+setClass("PDFToXMLPage", contains = c("ConvertedPDFPage", "DocumentPage"))
+
+
+setOldClass(c("PDFTextBoundingBox", "TextBoundingBox", "BoundingBox"))
+} 
 
 setAs("character", "PDFToXMLDoc", function(from) readPDFXML(from))
 
@@ -10,6 +21,6 @@ setAs("XMLInternalNode", "PDFToXMLPage",
           if(xmlName(from) != 'page') 
               stop("XML node is not a <page>")
 
-          class(from) = c("PDFToXMLPage", "ConvertedPDFPage", class(from))
+          class(from) = c("PDFToXMLPage", "ConvertedPDFPage", "DocumentPage", class(from))
           from
       })
