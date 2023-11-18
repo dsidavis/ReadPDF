c78d73777d3fddcfbd51f21425db436db0a46ccf
Duncan Temple Lang <duncan@r-project.org>
2023-11-18 12:57:08 -0800

    fixed the system call by removing it. And read status, not ans

--- R/pdftohtml.R
+++ R/pdftohtml.R
@@ -6,7 +6,8 @@ function(x)
 
 convertPDF2XML =
     # Convert a PDF document to XML using stdout
-function(file, out = character(), args = c("-q", "-xml"), pdftohtml = getOption("PDFTOHTML", Sys.getenv("PDFTOHTML", 'pdftohtml')))
+function(file, out = character(), args = c("-q", "-xml"),
+             pdftohtml = getOption("PDFTOHTML", Sys.getenv("PDFTOHTML", 'pdftohtml')))
 {
       # -q - quiet
       # -xml - convert to xml
@@ -16,13 +17,19 @@ function(file, out = character(), args = c("-q", "-xml"), pdftohtml = getOption(
     else if(grepl("\\.xml$", out))
         out = gsub("\\.xml$", "", out)
     
-    cmd = sprintf("%s %s '%s' %s", pdftohtml, paste(args, collapse = " "), path.expand(file), if(length(out)) QQuote(out) else "")
+#    cmd = sprintf("%s %s '%s' %s", pdftohtml, paste(args, collapse = " "), path.expand(file), if(length(out)) QQuote(out) else "")
+    #    ans = system(cmd, intern = TRUE)
+#    ans = system2(pdftohtml, c(args, shQuote(path.expand(file)), if(length(out)) shQuote(out) else character()))
+    
+    args2 = c(args, shQuote(file))
+    if(length(out))
+        args2 = c(args2, shQuote(out) )
+    status = system2(pdftohtml, args2, stdout = TRUE)
 
-    ans = system(cmd, intern = TRUE)
     if(length(out))
         return(paste0(out, ".xml"))
 
-    doc = xmlParsePDFTOHTML(ans, asText = TRUE)
+    doc = xmlParsePDFTOHTML(status, asText = TRUE)
     docName(doc) = file
     
     doc
